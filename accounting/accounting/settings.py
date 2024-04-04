01c04273225a4081300751c57e66268f5655dda7
djivkovic <92207833+djivkovic@users.noreply.github.com>
2024-04-04 15:33:55 +0200

    added accounting_db

--- accounting/accounting/settings.py
+++ accounting/accounting/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
+    "accounting_ms",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "accounting.urls"
@@ -75,8 +79,10 @@ WSGI_APPLICATION = "accounting.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'accounting_db',
+        "USER": 'root',
+        "PASSWORD": 'root'
     }
 }
 
@@ -119,5 +125,8 @@ STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- accounting/accounting_ms/models.py
+++ accounting/accounting_ms/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class pricingPlan(models.Model):
+    pricing_plan_name = models.CharField(max_length=100)
+    pricing_plan_adv = models.CharField(max_length=100)
+    pricing_plan_price = models.CharField(max_length=100)
+    pricing_plan_creator = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return 'name: ' + self.pricing_plan_name +', adv: ' + self.pricing_plan_adv+', price: ' + self.pricing_plan_price+', creator:' + self.pricing_plan_creator
