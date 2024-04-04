035365986344803c2cc9385a2ffa943520834431
djivkovic <92207833+djivkovic@users.noreply.github.com>
2024-04-04 16:43:57 +0200

    created skeleton for django

--- accounting/accounting_ms/views.py
+++ accounting/accounting_ms/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import pricingPlan
+
 
 # Create your views here.
+
+class PricingPlanView(APIView):
+    def get(self, request):
+        print('dobar dan')
+        return Response({"message":"success"})
+    
+    def post(self, request):
+        pass
